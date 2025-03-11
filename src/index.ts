cce6f3e1dbe0dda0e2e8721df04682e86c10d903
rohit21755 <rohit.k09022002@gmail.com>
2025-03-11 18:52:01 +0530

    user auth added

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import paymentRoutes from "./routes/payment";
 import { configDotenv } from "dotenv";
+import userRoute from "./routes/user";
 configDotenv();
 const app = express();
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use("/payment", paymentRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/user", userRoute);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
