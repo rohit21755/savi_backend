18dc00058c304703ee224588a696e0092496ad26
HARDIK-TSH1392 <hardik20507@iiitd.ac.in>
2025-04-01 02:48:52 +0530

    Added admin and user routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { createAdmin, loginAdmin, deleteProduct, deleteVariant, updateProductSale } from '../controllers/adminController';
+import { createAdmin, loginAdmin, deleteProduct, deleteVariant, updateProductSale, updateProductPrice, updateProductChangeStatus, getOrdersByState, updateOrderState } from '../controllers/adminController';
 import { isAdminAuthenticated } from "../middlewares/admin";
 import { z } from 'zod';
 const router = express.Router();
@@ -8,5 +8,9 @@ router.post('/signin', loginAdmin);
 router.delete("/product/:id", isAdminAuthenticated, deleteProduct);
 router.delete("/variant/:id", isAdminAuthenticated, deleteVariant);
 router.put("/product/sale/:id", isAdminAuthenticated, updateProductSale);
+router.put("/product/price/:id", isAdminAuthenticated, updateProductPrice);
+router.put("/product/change/:id", isAdminAuthenticated, updateProductChangeStatus);
+router.get('/orders-by-state', isAdminAuthenticated, getOrdersByState);
+router.put('/order/:id/state', isAdminAuthenticated, updateOrderState);
 
 export default router;
